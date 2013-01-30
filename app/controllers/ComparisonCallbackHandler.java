2df70706b5931666720f1ec0b32750bbfaadfbd8
Zhaidarbek Ayazbayev <zhaidarbek@gmail.com>
2013-01-30 16:20:04 +0600

    add dummy handler to test Comparison API callbacks

---
+++ app/controllers/ComparisonCallbackHandler.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import play.mvc.BodyParser;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class ComparisonCallbackHandler extends Controller {
+	
+	@BodyParser.Of(value = BodyParser.TolerantText.class)
+	public static Result index() {
+		System.out.println("Method: " + request().method());
+		System.out.println("URL: " + request().uri());
+		System.out.println("Headers: " + flattenMap(request().headers()));
+		System.out.println("Body: " + request().body().asText());
+		System.out.println("");
+		
+		return ok();
+	}
+	
+	private static Map<String, String> flattenMap(Map<String, String[]> headers){
+		Map<String, String> temp = new HashMap<String, String>();
+				
+		for(String header : headers.keySet()){
+			temp.put(header, request().getHeader(header));
+		}
+		
+		return temp;
+	}
+}
