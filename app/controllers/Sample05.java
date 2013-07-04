293dbd14b3a39e03cc977212eabcf0261edd0433
aleksey.permyakov <aleksey.permyakov@groupdocs.com>
2013-07-04 13:25:42 +0300

    Updated sample 5 according to PHP sample 5.

--- app/controllers/Sample05.java
+++ app/controllers/Sample05.java
@@ -1,26 +1,28 @@
 //###<i>This sample will show how to use <b>MoveFile</b> method from Storage Api to copy/move a file in GroupDocs Storage </i>
 package controllers;
 //Import of necessary libraries
+import java.io.FileInputStream;
 import java.util.List;
 import java.util.Map;
 
+import com.groupdocs.sdk.common.ApiInvoker;
+import com.groupdocs.sdk.common.FileStream;
+import com.groupdocs.sdk.common.GroupDocsRequestSigner;
+import com.groupdocs.sdk.model.*;
+import common.Utils;
 import models.Credentials;
 
 import org.apache.commons.lang3.StringUtils;
 
 import play.data.Form;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
 import scala.actors.threadpool.Arrays;
 
-import com.groupdocs.sdk.api.DocApi;
 import com.groupdocs.sdk.api.StorageApi;
 import com.groupdocs.sdk.common.ApiException;
-import com.groupdocs.sdk.common.ApiInvoker;
-import com.groupdocs.sdk.common.GroupDocsRequestSigner;
-import com.groupdocs.sdk.model.FileMoveResponse;
-import com.groupdocs.sdk.model.FileMoveResult;
-import com.groupdocs.sdk.model.GetDocumentInfoResponse;
 
 public class Sample05 extends Controller {
 	//###Set variables
@@ -31,67 +33,84 @@ public class Sample05 extends Controller {
 		FileMoveResult moveResult = null;
 		Form<Credentials> filledForm;
 		String sample = "Sample5";
+        String uploadDir = "";
+        String action = "";
 		Status status;
 		//Check POST parameters
 		if(request().method().equalsIgnoreCase("POST")){
 			filledForm = form.bindFromRequest();
 			if(filledForm.hasErrors()){
-				status = badRequest(views.html.sample05.render(title, sample, moveResult, filledForm));
+				status = badRequest(views.html.sample05.render(title, sample, moveResult, filledForm, action));
 			} else {
 				//Get POST data
 				Credentials credentials = filledForm.get();
 				session().put("client_id", credentials.client_id);
 				session().put("private_key", credentials.private_key);
 				session().put("server_type", credentials.server_type);
-				
-				Map<String, String[]> formData = request().body().asFormUrlEncoded();
-				String srcPath = formData.get("srcPath") != null ? formData.get("srcPath")[0] : null;
-				srcPath = StringUtils.isBlank(srcPath) ? null : srcPath.trim();
-				String destPath = formData.get("destPath") != null ? formData.get("destPath")[0] : null;
-				destPath = StringUtils.isBlank(destPath) ? null : destPath.trim();
-				boolean isCopy;
-				//Check what button was pressed copy or move
-				if(formData.containsKey("move")){
-					isCopy = false;
-				} else {
-					isCopy = true;
-				}
-		        
-				try {
-					//Check source and destination path 
-					if(srcPath == null || destPath == null){
-						throw new Exception();
-					}
-					//###Create ApiInvoker, Doc and Storage Api objects
-					
-					//Create ApiInvoker object
-					ApiInvoker.getInstance().setRequestSigner(
-							new GroupDocsRequestSigner(credentials.private_key));
-					//Create Doc Api object and get document metadata
-					GetDocumentInfoResponse metadata = new DocApi().GetDocumentMetadataByPath(credentials.client_id, srcPath);
-					Long fileId = null;
-					//Check request result
-					if(metadata != null && metadata.getStatus().trim().equalsIgnoreCase("Ok")){
-						fileId = metadata.getResult().getId().longValue();
-					} else {
-						throw new Exception("Not Found");
-					}
-					//Create Storage object
-					StorageApi api = new StorageApi();
-					api.setBasePath(credentials.server_type);
-					FileMoveResponse response;
-					//###Make a request to Storage API using clientId
-					if(isCopy){
-						response = api.MoveFile(credentials.client_id, destPath, null, fileId.toString(), null);
-					} else {
-						response = api.MoveFile(credentials.client_id, destPath, null, null, fileId.toString());
-					}
-					//Check request result
-					if(response != null && response.getStatus().trim().equalsIgnoreCase("Ok")){
-						moveResult = response.getResult();
-					}
-					//If request was successfull - set moveResult variable for template
-					status = ok(views.html.sample05.render(title, sample, moveResult, filledForm));
+                try {
+                    String guid = null;
+                    String fileName = null;
+                    Double fileId = 0d;
+                    ApiInvoker.getInstance().setRequestSigner(
+                            new GroupDocsRequestSigner(credentials.private_key));
+                    StorageApi api = new StorageApi();
+                    api.setBasePath(credentials.server_type);
+                    Http.MultipartFormData multipartFormData = request().body().asMultipartFormData();
+                    Map<String, String[]> formData = multipartFormData.asFormUrlEncoded();
+                    String sourse = Utils.getFormValue(formData, "sourse");
+                    guid = Utils.getFormValue(formData, "srcPath");
+                    if ("local".equals(sourse)){
+                        Http.MultipartFormData.FilePart filePart = multipartFormData.getFile("file");
+                        FileInputStream is = new FileInputStream(filePart.getFile());
+                        UploadResponse response = api.Upload(credentials.client_id, uploadDir + filePart.getFilename(), "comment", "", new FileStream(is));
+
+                        if (response != null && "Ok".equalsIgnoreCase(response.getStatus())) {
+                            guid = response.getResult().getGuid();
+                        }
+                        else {
+                            throw new Exception(response.getError_message());
+                        }
+                    }
+                    else if ("url".equals(sourse)){
+                        String url = Utils.getFormValue(formData, "url");
+                        UploadResponse response = api.UploadWeb(credentials.client_id, url);
+
+                        if (response != null && "Ok".equalsIgnoreCase(response.getStatus())) {
+                            guid = response.getResult().getGuid();
+                            fileName = url.split("/")[url.split("/").length - 1];
+                            fileId = response.getResult().getId();
+                        }
+                        else {
+                            throw new Exception(response.getError_message());
+                        }
+                    }
+                    String destPath = Utils.getFormValue(formData, "destPath");
+                    if (StringUtils.isEmpty(guid) || destPath == null){
+                        throw new Exception();
+                    }
+
+                    ListEntitiesResponse response =  api.ListEntities(credentials.client_id, uploadDir, 0, null, null, null, null, null, null);
+                    for (FileSystemDocument document : response.getResult().getFiles()){
+                        if (guid.equals(document.getGuid())){
+                            fileName = document.getName();
+                            fileId = document.getId();
+                            break;
+                        }
+                    }
+                    if (fileName == null || fileId == null){
+                        throw new Exception();
+                    }
+                    String copyToPath = (destPath + "/" + fileName).replaceAll("//", "/");
+                    FileMoveResponse moveResponse = null;
+                    if (Utils.getFormValue(formData, "copy") != null){
+                        moveResponse = api.MoveFile(credentials.client_id, copyToPath, null, Double.toString(fileId), null);
+                        action = "copy";
+                    }
+                    else if (Utils.getFormValue(formData, "move") != null){
+                        moveResponse = api.MoveFile(credentials.client_id, copyToPath, null, null, Double.toString(fileId));
+                        action = "move";
+                    }
+                    status = ok(views.html.sample05.render(title, sample, moveResponse.getResult(), filledForm, action));
 				//###Definition of Api errors and conclusion of the corresponding message
 				} catch (ApiException e) {
 					if(e.getCode() == 401){
@@ -100,23 +119,17 @@ public class Sample05 extends Controller {
 					} else {
 						filledForm.reject("Failed to access API: " + e.getMessage());
 					}
-					status = badRequest(views.html.sample05.render(title, sample, moveResult, filledForm));
+					status = badRequest(views.html.sample05.render(title, sample, moveResult, filledForm, action));
 			    //###Definition of filledForm errors and conclusion of the corresponding message	
 				} catch (Exception e) {
-					if(srcPath == null){
-						filledForm.reject("srcPath", "This field is required");
-					} else if(destPath == null){
-						filledForm.reject("destPath", "This field is required");
-					} else {
-						filledForm.reject("srcPath", "Something wrong with your file: " + e.getMessage());
-					}
-					status = badRequest(views.html.sample05.render(title, sample, moveResult, filledForm));
+                    filledForm.reject("srcPath", "Something wrong: " + e.getMessage());
+					status = badRequest(views.html.sample05.render(title, sample, moveResult, filledForm, action));
 				}
 			}
 		} else {
 			filledForm = form.bind(session());
 			session().put("server_type", "https://api.groupdocs.com/v2.0");
-			status = ok(views.html.sample05.render(title, sample, moveResult, filledForm));
+			status = ok(views.html.sample05.render(title, sample, moveResult, filledForm, action));
 		}
 		//Process template
 		return status;
