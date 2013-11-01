647b092f8f0bcf093b90f7a8bec6386cb6b90ca8
Alex Bobkov <lilalex85@gmail.com>
2013-11-01 18:35:20 +0200

    Revert "updated for play 2.2.1"
    This reverts commit e8cdf72fc486415e06b0b43ccf3320f946a1547a.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 
-This sample was developed using Play 2.2.1.
+This sample was developed using Play 2.0.4.
 
 To see online demo of this project please go to [http://groupdocs-java-samples.herokuapp.com/](http://groupdocs-java-samples.herokuapp.com/)
 
 To deploy project locally follow this instructions:
 
-	1. Download and install Play 2.2.1 from http://www.playframework.org/download/
+	1. Download and install Play 2.0.4 from http://www.playframework.org/download/
 	2. Open console and run: `git clone https://github.com/groupdocs/groupdocs-java-samples`
 	3. cd to project directory and run: `play "run 8080"`
 	4. Now open your browser and go to http://localhost:8080/
--- project/Build.scala
+++ project/Build.scala
@@ -1,11 +1,11 @@
 import sbt._
 import Keys._
-import play.Project._
+import PlayProject._
 
 object ApplicationBuild extends Build {
 
     val appName         = "groupdocs-java-samples"
-    val appVersion      = "1.8.0"
+    val appVersion      = "1.7.0"
 
     val appDependencies = Seq(
         "com.sun.jersey" % "jersey-core" % "1.13",
@@ -14,10 +14,10 @@ object ApplicationBuild extends Build {
         "org.apache.commons" % "commons-io" % "1.3.2",
         "com.sun.jersey" % "jersey-client" % "1.9.1",
         "com.groupdocs" % "groupdocs-java-client" % "1.7.0",
-        "com.typesafe" %% "play-plugins-mailer" % "2.1-RC2"
+        "com.typesafe" %% "play-plugins-mailer" % "2.0.4"
     )
 
-    val main = play.Project(appName, appVersion, appDependencies).settings(
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
         resolvers += "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository",
         templatesImport += "com.groupdocs.sdk.model._",
         templatesImport += "com.groupdocs.sdk.common._"
